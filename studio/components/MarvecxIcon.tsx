ce2b6f6e5c1626c2a9150943f0aac8eda5d2dd4f
Legaljay <adesinaayobami67@gmail.com>
2026-08-21 20:55:41 +0100

    chore: testing sanity icon change, styling

--- studio/components/MarvecxIcon.tsx
+++ studio/components/MarvecxIcon.tsx
@@ -4,7 +4,9 @@ export function MarvecxIcon() {
     <img
       src="/static/favicon.svg"
       alt="Marvecx"
-      style={{ width: "1em", height: "1em" }}
+      width={25}
+      height={25}
+      style={{ width: "1em", height: "1em", objectFit: "contain" }}
     />
   );
 }
